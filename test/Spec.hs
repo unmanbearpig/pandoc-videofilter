eba97f1bde1fb9ca2d1e838e566ac6f95abf42fa
unmanbearpig <unmanbearpig@gmail.com>
2022-05-31 10:38:09 +0300

    Fix tests
    - Update test dependencies
    - Switch from `String` to `Text`
    - The order of HTML attributes has changed for some reason, so update it
      in tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Test.Hspec
 import Text.Pandoc.Definition (Format(..), Inline(..), Attr(), nullAttr)
 import WrapperElement (WrapperElement(..))
@@ -12,21 +14,21 @@ main = hspec $ do
       inlineHtmlVideo Nothing NoWrapper (Image nullAttr [] ("vimeo:123", ""))
         `shouldBe` (Just $
                      RawInline (Format "html")
-                     "<iframe allowfullscreen mozallowfullscreen frameborder=\"0\" src=\"https://player.vimeo.com/video/123?title=0&amp;amp;portrait=0&amp;amp;badge=0\" webkitallowfullscreen></iframe>")
+                     "<iframe src=\"https://player.vimeo.com/video/123?title=0&amp;amp;portrait=0&amp;amp;badge=0\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>")
     it "uses provided default dimensions" $ do
       inlineHtmlVideo (Just (10, 20)) NoWrapper (Image nullAttr [] ("vimeo:123", ""))
         `shouldBe` (Just $
                      RawInline (Format "html")
-                     "<iframe height=\"20\" width=\"10\" allowfullscreen mozallowfullscreen frameborder=\"0\" src=\"https://player.vimeo.com/video/123?title=0&amp;amp;portrait=0&amp;amp;badge=0\" webkitallowfullscreen></iframe>")
+                     "<iframe src=\"https://player.vimeo.com/video/123?title=0&amp;amp;portrait=0&amp;amp;badge=0\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen width=\"10\" height=\"20\"></iframe>")
     it "overrides default dimensions" $ do
       inlineHtmlVideo Nothing NoWrapper (Image nullAttr [(Str "40x50")] ("vimeo:123", ""))
         `shouldBe` (Just $
                      RawInline (Format "html")
-                     "<iframe height=\"50\" width=\"40\" allowfullscreen mozallowfullscreen frameborder=\"0\" src=\"https://player.vimeo.com/video/123?title=0&amp;amp;portrait=0&amp;amp;badge=0\" webkitallowfullscreen></iframe>")
+                     "<iframe src=\"https://player.vimeo.com/video/123?title=0&amp;amp;portrait=0&amp;amp;badge=0\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen width=\"40\" height=\"50\"></iframe>")
     it "wraps it in a div" $ do
       inlineHtmlVideo Nothing (DivWithClass "class1 class2") (Image nullAttr [] ("vimeo:123", ""))
         `shouldBe` (Just $
                      RawInline (Format "html")
-                     "<div class=\"class1 class2\"><iframe allowfullscreen mozallowfullscreen frameborder=\"0\" src=\"https://player.vimeo.com/video/123?title=0&amp;amp;portrait=0&amp;amp;badge=0\" webkitallowfullscreen></iframe></div>")
+                     "<div class=\"class1 class2\"><iframe src=\"https://player.vimeo.com/video/123?title=0&amp;amp;portrait=0&amp;amp;badge=0\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>")
     it "doesn't wrap when no video" $ do
       inlineHtmlVideo Nothing (DivWithClass "testclass") (Str "hello") `shouldBe` (Nothing)
